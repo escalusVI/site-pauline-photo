518e1f2606e1c5d84a22b1f163e4d143bc366640
escalusVI <loicvaleryvi@gmail.com>
2017-01-03 22:43:25 +0100

    ajout header et mise en forme du menu

---
+++ include/header.php
@@ -0,0 +1,16 @@
+<img src="images/logoHeader.png">
+
+<nav>
+	<ul id="menu">
+		<li><a href="index.php">ACCUEIL</a></li>
+		<li><a href="#">A PROPOS</a></li>
+		<li><a href="#">MARIAGE</a></li>
+		<li><a href="#">PORTRAIT</a></li>
+		<li><a href="#">FORMATION</a></li>
+		<li><a href="#">PAUSE PHOTO</a></li>
+		<li><a href="#">TRAVAIL D'AUTEUR</a></li>
+		<li><a href="#">LINETTE</a></li>
+		<li><a href="#">ACCES CLIENT</a></li>
+		<li><a href="#">CONTACT</a></li>
+	</ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="style.css">
+		<title>Index</title>
+	</head>
+	
+	<body>
+		<header>
+			<?php include ("include/header.php");?>
+		</header>
+	</body>
+	
+</html>
