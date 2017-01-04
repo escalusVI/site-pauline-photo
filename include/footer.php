4a8ac4c14b2f098ca07d4499d4913a8f2000689f
escalusVI <loicvaleryvi@gmail.com>
2017-01-04 01:09:50 +0100

    ajout du footer

---
+++ include/footer.php
@@ -0,0 +1,6 @@
+<img src="images/logoFooter.png">
+<img src="images/facebookFooter.png">
+<img src="images/twitterFooter.png">
+<img src="images/youtubeFooter.png">
+<img src="images/instagram.png">
+<div id="ligneFooter"></div>
