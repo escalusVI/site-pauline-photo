1b871dfce7dbb2140822af9cd905b04ef9eb35b7
escalusVI <loicvaleryvi@gmail.com>
2017-02-07 15:53:58 +0100

    debut de la mise en page a propos, probleme au niveau du post-it

--- aPropos.php
+++ aPropos.php
@@ -11,13 +11,38 @@
 		
 			<div class="container">
 			
+			<header class="col-lg-offset-1 col-lg-10">
 				<?php include ("include/header.php");?>
+			</header>
 			
-			
-			<section>
-			
+			<section class="row col-lg-offset-1 col-lg-10">
+				<article class="col-lg-6 propos">
+					
+					<div class="note">
+						<p class="col-lg-offset-3 noteText">texte note</p>
+						<img src="images/aPropos/note.png" class="col-lg-offset-3 postIt">
+					</div>
+					<h1></br>PAULINE PETIT</h1>
+					<p>
+						PHOTOGRAPHE PRO</br>
+						DIPLOMEE CAP ET BAC PRO PHOTO</br>
+						NIKONISTE (D750)</BR>
+						YOUTUBEUSE (<a href="https://www.youtube.com/channel/UC5mXEYHSl4xjmG2ZKfBGyjw" target="_blank">CHAINE PAUSE PHOTO</a>)</br>
+						100% PASSIONNEE</br>
+						<img src="images/aPropos/appareil.png" class="appareil">
+						STYLE ORIGINAL</br>
+						APPROCHE HUMAINE
+						
+					</p>
+						
+				</article>
+				
+				<article class="visible-lg col-lg-6 portrait">
+					<img src="images/aPropos/portrait.png">
+				</article>
+				
 			</section>
-			<footer>
+			<footer class="col-lg-offset-1 col-lg-10">
 				<?php include ("include/footer.php");?>
 			</footer>
 			
