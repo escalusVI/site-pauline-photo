3e83aa490ad741fe8a4dc023e201a55fdf7348b6
escalusVI <loicvaleryvi@gmail.com>
2017-02-28 08:52:24 +0100

    modification page index avec carousel

--- index.php
+++ index.php
@@ -4,40 +4,61 @@
 		<meta charset="utf-8">
 		<link rel="stylesheet" href="style.css">
 		<link rel="stylesheet" href="styleIndex.css">
-		<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
-		<title></title>
+		<link rel="stylesheet" href="carousel/style.css">
+		<link rel="stylesheet" href="carousel/css/bootstrap.css">
+		<title>Index</title>
 	</head>
 	
-	<body>
-		
-			<div class="container">
-		
-		<header class="col-lg-offset-1 col-lg-10">
-				<?php include ("include/header.php");?>
-			
+	<div id="html">
+	<body class="container">
+		<header>
+			<?php include ("include/header.php");?>
 		</header>
 		
-		<section class="row col-lg-offset-1 col-lg-10">
-			
-		<article class="col-lg-8 mariage">
-			
+		<section>
+		<!--ajout du carousel-->
+			<div class="carousel fade-carousel slide" data-ride="carousel" data-interval="4000" id="bs-carousel">
+
+	<!-- Indicators -->
+	<ol class="carousel-indicators">
+		<li data-target="#bs-carousel" data-slide-to="0" class="active"></li>
+		<li data-target="#bs-carousel" data-slide-to="1"></li>
+		<li data-target="#bs-carousel" data-slide-to="2"></li>
+	 </ol>
+  
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item slides active">
+      <a href="#"><div class="slide-1"></div></a>
+    </div>
+    <div class="item slides">
+      <a href="#"><div class="slide-2"></div></a>               
+    </div>
+    <div class="item slides">
+      <a href="#"><div class="slide-3"></div></a>       
+        
+    </div>
+  </div> 
+</div>
+
+	<script src="carousel/js/jquery.js"></script>
+	<script src="carousel/js/bootstrap.min.js"></script>
+<!--fin du carousel-->
+		
+		<article id="mariage">
 			<img src="images/coeur.png"/ id="coeur" >
 			<a href=""><img src="images/mariage.png" id="couple"/></a>
 			<img src="images/photographe.png" id="photographe"/>
+			
 			<div>
 				<p></br>REPORTAGE PHOTOGRAPHIQUE-PLUSIEURS FORFAITS DES PREPARATIFS AU DESSERT-A PARTIR DE 400 €<img src="images/coeurBlanc.png" id="coeurBlanc"/></p>
 			</div>
 			
 		</article>
-		
-		<aside class="col-lg-offset-1 col-lg-3">
-		<?php include ("include/aside.php");?>
-		</aside>
-		<!--
-		<article class="col-col-8 portrait">
-		
+
+		<article id="portrait">
 			<div id="ligne">
-				<img src="images/parapluie.png" id="parapluie">
+			<img src="images/parapluie.png" id="parapluie">
 				<h1>PORTRAIT EN STUDIO</h1>
 			</div>
 			
@@ -46,10 +67,9 @@
 			<div>
 				<p>NOUVEAU-NES-BEBES-ENFANTS-FAMILLE</br>COUPLES-LINGERIE-GROSSESSE</br>PLUSIEURS FORFAITS-A PARTIR DE 42 €</p>
 			</div>
-			
 		</article>
 		
-		<article class="col-md-8 articleLaine">
+		<article id="articleLaine">
 			<div>
 				<h1>FORMATION PHOTO</h1><img src="images/formation.png" id="formation"/>
 			</div>
@@ -57,20 +77,20 @@
 				<p>COURS PHOTO INDIVIDUEL-COACHING PHOTO</BR>ATELIER PHOTO COLLECTIF-SORTIE PHOTO</BR>CHAINE YOUTUBE EDUCATIVE-VIDEOS ET TUTOS</BR>A PARTIR DE 20 €</p>
 				<img src="images/focus.png" id="focus"/>
 			</div>
-		<a href="#"><img src="images/laine.png" id="laine"/></a>
+			<a href="#"><img src="images/laine.png" id="laine"/></a>
+		</article>
 		
-		</article>-->
+		<aside>
+		<?php include ("include/aside.php");?>
+		</aside>
 			
-			</section>
-			
-			<footer class="col-lg-offset-1 col-lg-10">
-				<?php include ("include/footer.php");?>
-			</footer>
-			
-			</div>
+		</section>
 		
-		<script src="bootstrap/js/bootsrap.js"></script>
-		<script src="bootstrap/js/jquery.js"></script>
+		
+		 
+		<footer>
+			<?php include ("include/footer.php");?>
+		</footer>
 	</body>
-	
+	</div>
 </html>
